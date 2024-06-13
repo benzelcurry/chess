6df60ed8ffeb87cb15ccf77e47c7c39707848252
Ben <benawhiting@gmail.com>
2024-06-13 16:07:07 -0700

    Refactor #draw_board to use more maintainable logic

--- lib/board.rb
+++ lib/board.rb
@@ -1,35 +1,49 @@
 # TODOs:
-# 1. Replace draw_board with something that uses the same design but operates differently
-# 2. Have it check for pieces (whose positions will be established by default, will need to decide if pieces stored in Board or Player)
+# 1. Make board a 2D array of 8 subarrays of length 8
+# 2. Arrays will be prepopulated with the pieces
 # 3. Make sure it uses the unicode chesspieces accordingly
+# 4. Make a piece class that's extended by respective pieces?
 
 class Board
+  attr_accessor :board
+
+  X_AXIS = (1..8).to_a.freeze
+  Y_AXIS = ('A'..'H').to_a.freeze
+
+  def initialize
+    self.board = Array.new(7) { Array.new(8, '_') }
+  end
+
+  # Prints the board to the console
   def draw_board
-    columns = ('A'..'H').to_a
-    rows = (1..8).to_a.reverse
-    board = ""
-  
-    # Print column headers
-    board += "  " + columns.join(" ") + "\n"
-  
-    # Print each row with its label
-    rows.each do |row|
-      board += row.to_s + " " # Row label
-      columns.each_with_index do |col, index|
-        # Determine whether to print a black or white square
-        if (row + index).even?
-          board += "█" # Black square
-        else
-          board += " " # White square
+    board.each_with_index do |row, i|
+      row_string = ''
+      row_string += "#{Y_AXIS[i]}  "
+      if i.even?
+        row.each_index do |n|
+          if n.even?
+            row_string += ' '
+          else
+            row_string += '█'
+          end
         end
-        board += " " # Space between squares
+      else
+        row.each_index do |n|
+          if n.odd?
+            row_string += ' '
+          else
+            row_string += '█'
+          end
+        end
       end
-      board += row.to_s + "\n" # Repeat row label at the end of the row
+
+      puts row_string
     end
-  
-    # Print column headers again at the bottom
-    board += "  " + columns.join(" ") + "\n"
-  
-    puts board
+
+    x_labels = "\n   "
+    X_AXIS.each do |i|
+      x_labels += i.to_s
+    end
+    puts x_labels
   end
 end
