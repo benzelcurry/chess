19da788fac3d386a321a8e28a8b7a27d68fc6d13
Ben <benawhiting@gmail.com>
2024-06-20 15:37:46 -0700

    Set icon color depending on color passed to piece

--- lib/main.rb
+++ lib/main.rb
@@ -4,3 +4,9 @@ require_relative('pieces/pawn')
 
 board = Board.new
 board.draw_board
+
+white_pawn = Pawn.new('white', 'below')
+black_pawn = Pawn.new('black', 'above')
+
+puts white_pawn.icon
+puts black_pawn.icon
--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,12 +2,20 @@
 class Pawn < Piece
   # TODO: Add a property that stores the unicode pawn character
   # Moves the pawn to its desired location
+
+  attr_accessor :icon
+
+  def initialize(color, location)
+    super(color, location)
+    self.icon = (color == 'white' ? '♙' : '♟')
+  end
+
   def move
     # Split the above input gathering into its own method within Piece
-    puts 'Please enter which piece you would like to move'
-    puts 'Please enter where you\'d like to move it'
-    destination = ''
-    until valid_destination?(destination) { destination = gets.chomp.downcase }
+    # puts 'Please enter which piece you would like to move'
+    # puts 'Please enter where you\'d like to move it'
+    # destination = ''
+    # until valid_destination?(destination) { destination = gets.chomp.downcase }
 
   end
 end
