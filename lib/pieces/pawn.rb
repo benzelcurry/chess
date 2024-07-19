077a175af159aa3788d5c77c9d228e6e01e8109c
Ben <benawhiting@gmail.com>
2024-07-19 14:03:26 -0700

    Add logic for basic pawn movements

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,24 +1,23 @@
 # Pawn; extends Piece
 class Pawn < Piece
-  # TODO: Add a property that stores the unicode pawn character
-  # Moves the pawn to its desired location
-
   attr_accessor :icon, :name
 
   def initialize(color, location)
     super(color, location)
     self.icon = (color == 'white' ? '♙' : '♟')
     self.name = 'Pawn'
+    self.first_turn = true
   end
 
   # Defines whether the passed move is a legal move for a pawn
   # TODO: Fix this and read how pawns should be allowed to move (will be different based on color)
   def legal_move?(destination)
-    if (destination[0] == location[0] + 1 || destination[0] == location[1] - 1) &&
-       (destination[1] == location[1] + 1 || destination[1] == location[1] - 1)
-      true
+    x_dif = destination[0] - location[0]
+
+    if first_turn
+      x_dif <= 2 && x_dif.positive? ? true : false
     else
-      false
+      x_dif <= 1 && x_dif.positive? ? true : false
     end
   end
 end
