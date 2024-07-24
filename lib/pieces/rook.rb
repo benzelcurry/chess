3c144a8e8c19d65863e1a9af3c8a1eeb77d5c853
Ben <benawhiting@gmail.com>
2024-07-24 15:14:23 -0700

    Write legal move logic for rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -10,6 +10,9 @@ class Rook < Piece
 
   # Defines whether the passed move is a legal move for a rook
   def legal_move?(destination, target)
-    # if x changes, y doesnt and vice versa
+    x_dif = destination[0] - location[0]
+    y_dif = destination[1] - location[1]
+
+    x_dif.zero? || y_dif.zero? ? true : false
   end
 end
