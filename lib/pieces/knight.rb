6c135916a5e6045715be6547a367e3e50e27199f
Ben <benawhiting@gmail.com>
2024-08-28 16:46:38 -0700

    Initialize a knight class

---
+++ lib/pieces/knight.rb
@@ -0,0 +1,19 @@
+# Knight; extends Piece
+class Knight < Piece
+  attr_accessor :icon, :name
+
+  def initialize(color, location)
+    super(color, location)
+    self.icon = (color == 'white' ? '♘' : '♞')
+    self.name = 'Knight'
+  end
+
+  # Defines whether the passed move is a legal move for a knight
+  def legal_move?(destination, _target)
+    x_dif = (destination[0] - location[0]).abs
+    y_dif = (destination[1] - location[1]).abs
+    split = (x_dif - y_dif).abs
+
+    split == 1
+  end
+end
