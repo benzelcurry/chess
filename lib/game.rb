a980dbce9e5f6a565ae2e4813ae960a7b50ca231
Ben <benawhiting@gmail.com>
2024-08-29 15:56:20 -0700

    Initialize Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,9 @@
-# TODO: Write out a class once the other classes are done
+# Class for running the chess game
+class Game
+  attr_accessor :turn, :turns_taken
+
+  def initialize
+    self.turn = 0
+    self.turns_taken = 0
+  end
+end
