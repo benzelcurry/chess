f666cabd3e1667fdf620ed93f5a4e0f98194b5b1
Benzel <benawhiting@gmail.com>
2024-09-21 13:20:31 -0700

    Begin adding methods and pseudocode to check if king is in check

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,3 +1,5 @@
+require_relative('board')
+
 # King; extends Piece
 class King < Piece
   attr_accessor :icon, :name
@@ -15,4 +17,49 @@ class King < Piece
 
     x_dif <= 1 && y_dif <= 1
   end
+
+  # Checks to see if king is currently in check
+  def in_check?
+    x = location[0]
+    y = location[1]
+
+    knight_spots = [
+      [x + 2, y + 1],
+      [x + 2, y - 1],
+      [x - 2, y + 1],
+      [x - 2, y - 1],
+      [x + 1, y + 2],
+      [x + 1, y - 2],
+      [x - 1, y + 2],
+      [x - 1, y - 2]
+    ]
+
+    # TODO: Check all knight_spots for knights of opposing color; if present, announce check
+
+    horizontal_blockers = [
+      board.check_path_horizontal(self, [0, y]),
+      board.check_path_horizontal(self, [7, y])
+    ]
+
+    # TODO: Check all horizontal_blockers for rooks and queens of opposing colors
+
+    vertical_blockers = [
+      board.check_path_vertical(self, [x, 0]),
+      board.check_path_vertical(self, [x, 7])
+    ]
+
+    # TODO: Check all horizontal_blockers for rooks and queens of opposing colors
+
+    diagonal_blockers = [
+      # TODO: Will need to do some math to calculate what the diagonal options relating to piece are
+    ]
+
+    # TODO: Checkk all diagonal_blockers for pawns, bishops, and queens of opposing colors
+    # NOTE: Pawns can only capture depending on which direction they're in
+  end
+
+  # TODO: Add a method that calls #in_check? for every possible move the king can make and then
+  #       stores them in a new variable for the piece. Then modify #legal_move? to exclude
+  #       moves that would fall in this list. End the game if the king is in checkmate. Write
+  #       messages that announce when a king is in check.
 end
