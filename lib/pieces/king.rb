4c452998b73dc80c0466f1a7c09ad3510b6ece52
Benzel <benawhiting@gmail.com>
2024-09-28 13:32:39 -0700

    Add logic to see if in check by bishop or queen

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -22,6 +22,7 @@ class King < Piece
   def in_check?(board)
     x = location[0]
     y = location[1]
+    opposite_color = color == 'white' ? 'black' : 'white'
 
     knight_spots = [
       [x + 2, y + 1],
@@ -35,7 +36,7 @@ class King < Piece
     ]
 
     knight_spots.each do |spot|
-      return true if board[spot[0]][spot[1]]&.name == 'knight'
+      return true if board[spot[0]][spot[1]]&.name == 'knight' && board[spot[0]][spot[1]]&.color == opposite_color
     end
 
     straight_blockers = [
@@ -46,12 +47,26 @@ class King < Piece
     ]
 
     straight_blockers.each do |blocker|
-      return true if blocker&.name == 'rook' || blocker&.name == 'queen'
+      return true if (blocker&.name == 'rook' || blocker&.name == 'queen') && blocker&.color == opposite_color
     end
 
+    diagonal_bounds = {
+      top_left: [x - [x, y].min, y - [x, y].min],
+      bottom_right: [x + [7 - x, 7 - y].min, y + [7 - x, 7 - y].min],
+      top_right: [x - [x, 7 - y].min, y + [x, 7 - y].min],
+      bottom_left: [x + [7 - x, y].min, y - [7 - x, y].min]
+    }
+
     diagonal_blockers = [
-      # TODO: Will need to do some math to calculate what the diagonal options relating to piece are
+      board.check_path_diagonal(self, diagonal_bounds.top_left),
+      board.check_path_diagonal(self, diagonal_bounds.top_right),
+      board.check_path_diagonal(self, diagonal_bounds.bottom_left),
+      board.check_path_diagonal(self, diagonal_bounds.bottom_left)
     ]
+
+    diagonal_blockers.each do |blocker|
+      return true if (blocker&.name == 'bishop' || blocker&.name == 'queen') && blocker&.color == opposite_color
+    end
 
     # TODO: Checkk all diagonal_blockers for pawns, bishops, and queens of opposing colors
     # NOTE: Pawns can only capture depending on which direction they're in
